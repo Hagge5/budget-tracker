8153939937bfae4aafb3a25a55fea3b82df211fa
hagge5 <hannes.haglund@gmail.com>
2016-05-21 21:47:05 +0200

    Modify week to funnel a weeks money to savings immediately if savings is below zero.

--- week.py
+++ week.py
@@ -16,6 +16,15 @@ class Week:
 
     def increase(self, amount):
         self.remainingMoney += amount
+        # We prioritize having savings above 0 over this weeks money
+        if self.savings < 0:
+            if self.remainingMoney > abs(self.savings):
+                self.remainingMoney += self.savings
+                self.savings = 0
+            else:
+                self.savings += self.remainingMoney
+                self.remainingMoney = 0
+        # If we have surplus money, funnel it to savings
         if self.remainingMoney > self.maxPerWeek:
             self.savings += self.remainingMoney - self.maxPerWeek
             self.remainingMoney = self.maxPerWeek
